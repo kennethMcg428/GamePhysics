a82f4db33951b328344b933517cdcda2f3727740
kennethMcg428 <kennethmcgalliard@yahoo.com>
2018-07-06 01:28:06 -0700

    Added physics and velocity newtons first law

---
+++ GamePhysics101/Assets/_Scripts/PhysicsEngine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour {
+
+    public Vector3 VelocityVector; // average velocity this FixedUpdate()
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Accelerate();
+    }
+    void Accelerate()
+    {
+        transform.position += VelocityVector * Time.deltaTime;
+    }
+}
