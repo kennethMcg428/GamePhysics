ba06cf0a2ef01c22e689d3964bd11197e92daa80
kennethMcg428 <kennethmcgalliard@yahoo.com>
2018-07-23 01:34:30 -0700

    refactored physics engine add forces through outside scripts started rocket engine script

--- GamePhysics101/Assets/_Scripts/PhysicsEngine.cs
+++ GamePhysics101/Assets/_Scripts/PhysicsEngine.cs
@@ -18,31 +18,67 @@ Air Resistance Force*
 Applied Force*
 Spring Force*
          */
-    //Trails
-    public bool showTrails = true;
-    private LineRenderer lineRenderer;
+    
     private int numberOfForces;
 
     //Force Vectors
-    private Vector3 FrictionalForce;
-    private Vector3 GravitationalForce;
-    private Vector3 AppliedForce;
-    private Vector3 NormalForce;
-    private Vector3 AirResistanceForce;
-    private Vector3 SpringForce;
-    public Vector3 netForceVector;
+   // private Vector3 FrictionalForce;
+   // private Vector3 GravitationalForce;
+   // private Vector3 AppliedForce;
+   // private Vector3 NormalForce;
+   // private Vector3 AirResistanceForce;
+   // private Vector3 SpringForce;
 
+    [HideInInspector]
     public List<Vector3> forceVectorList = new List<Vector3>();
-    public Vector3 VelocityVector; // average velocity this FixedUpdate()
 
-
-    public float Mass = 1;
+    public float Mass = 1;      // [kg]
+    public Vector3 VelocityVector; // [m s^-1]
+    public Vector3 netForceVector; // N [kg m s^-1]
+     //Trails
+    private LineRenderer lineRenderer;
+    public bool showTrails = true;
 
 	// Use this for initialization
 	void Start () {
         InitializeTrails();
 	}
-    private void Update()
+    
+    private void FixedUpdate()
+    {
+        RenderTrails();
+        UpdatePosition();
+    }
+
+    public void AddForce(Vector3 forceVector)
+    {
+        forceVectorList.Add(forceVector);
+    }
+
+    void UpdatePosition()
+    {
+        //Sum forces add clear list
+        netForceVector = Vector3.zero;
+        foreach(Vector3 forceVector in forceVectorList)
+            { netForceVector += forceVector; }
+        forceVectorList = new List<Vector3>();
+        // Calculate change in acceleration and velocity due to change in net force
+        Vector3 accelerationVector = netForceVector / Mass;
+        VelocityVector += accelerationVector * Time.deltaTime;
+        //Update position
+        transform.position += VelocityVector * Time.deltaTime;
+    }
+
+    void InitializeTrails()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.SetColors(Color.yellow, Color.yellow);
+        lineRenderer.SetWidth(0.2F, 0.2F);
+        lineRenderer.useWorldSpace = false;
+    }
+
+    private void RenderTrails()
     {
         if (showTrails)
         {
@@ -61,39 +97,5 @@ Spring Force*
         {
             lineRenderer.enabled = false;
         }
-    }
-    private void FixedUpdate()
-    {
-        AddForces();
-        UpdateVelocity();
-        transform.position += VelocityVector * Time.deltaTime;
-    }
-    void Accelerate()
-    {
-           UpdateVelocity();
-          
-    }
-    void AddForces()
-    {
-        netForceVector = Vector3.zero;
-        foreach(Vector3 forceVector in forceVectorList)
-            { netForceVector += forceVector; }
-    }
-    void UpdateVelocity()
-    {
-        Vector3 accelerationVector = netForceVector / Mass;
-        VelocityVector += accelerationVector * Time.deltaTime;
-    }
-    void InitializeTrails()
-    {
-        lineRenderer = gameObject.AddComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.SetColors(Color.yellow, Color.yellow);
-        lineRenderer.SetWidth(0.2F, 0.2F);
-        lineRenderer.useWorldSpace = false;
-    }
-    public List<Vector3> GetForceVectorList()
-    {
-        return forceVectorList;
     }
 }
