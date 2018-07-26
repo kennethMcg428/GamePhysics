cd4be1e26664c425d8a86e71d6a8d132e92a9d8f
kennethMcg428 <kennethmcgalliard@yahoo.com>
2018-07-25 21:20:34 -0700

    added missing files from earlier commits

---
+++ GamePhysics101/Assets/_Scripts/AddForce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhysicsEngine))]
+public class AddForce : MonoBehaviour {
+
+    private PhysicsEngine phsx;
+    public Vector3 forceVector; // [none]
+
+	// Use this for initialization
+	void Start () {
+        phsx = GetComponent<PhysicsEngine>();
+        
+	}
+
+    private void FixedUpdate()
+    {
+        phsx.AddForce(forceVector);
+    }
+
+}
+
---
+++ GamePhysics101/Assets/_Scripts/RocketEngine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhysicsEngine))]
+public class RocketEngine : MonoBehaviour {
+
+    public float fuelMass;          // [kg]
+    public float maxThrust;         // kN [kg m s^-2]
+
+    [Range(0,1)]
+    public float thrustPercent;     // [none]
+
+    private PhysicsEngine phsx;     
+    public Vector3 ThrustUnitVector;// [none]
+
+    private float currTrust;        // N
+
+    // Use this for initialization
+    void Start()
+    {
+        phsx = GetComponent<PhysicsEngine>();
+        phsx.Mass += fuelMass;
+    }
+
+    private void FixedUpdate()
+    {
+        if(fuelMass > FuelThisUpdate())
+        {
+            fuelMass -= FuelThisUpdate();
+            phsx.Mass -= FuelThisUpdate();
+            ExertForce();
+        }
+        else
+        {
+            Debug.LogWarning("out of fuel");
+        }
+    }
+
+    float FuelThisUpdate()
+    {
+        float exhaustMassFlow = 0 ;
+        float effectiveExhaustVelocity = 0;
+
+        exhaustMassFlow = currTrust / effectiveExhaustVelocity;
+
+        return exhaustMassFlow * Time.deltaTime;
+    }
+    void ExertForce()
+    {
+        currTrust = thrustPercent * maxThrust * 1000;
+        Vector3 thrustVector = ThrustUnitVector.normalized * currTrust;
+        phsx.AddForce(thrustVector);
+        
+    }
+}
