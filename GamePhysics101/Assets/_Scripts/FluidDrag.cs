801df2d1fc51c375faa7eb8f97ea6ead4b2ed4fa
kennethMcg428 <kennethmcgalliard@yahoo.com>
2018-07-26 18:15:08 -0700

    same as last

---
+++ GamePhysics101/Assets/_Scripts/FluidDrag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidDrag : MonoBehaviour {
+
+[Range(1, 2)]
+public float velocityExponent;
+
+public float dragConstant;
+
+    private PhysicsEngine physx;
+
+	// Use this for initialization
+	void Start () {
+        physx = GetComponent<PhysicsEngine>();
+	}
+	
+	// Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        Vector3 velocityVector = physx.VelocityVector;
+        float speed = velocityVector.magnitude;
+        float dragSize = CalculateDrag(speed);
+        Vector3 dragVector = dragSize * -velocityVector.normalized;
+        physx.AddForce(dragVector);
+    }
+    float CalculateDrag(float velocity)
+    {
+        return dragConstant * Mathf.Pow(velocity, velocityExponent);
+
+    }
+}
